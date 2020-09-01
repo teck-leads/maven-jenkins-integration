f8e00100444b80509acc688fdac1b02f5e7a84aa
TechLeads <anupoju.anand@gmail.com>
2020-09-01 14:45:27 +0530

    testcase is added

--- maven-jenkins-integration/src/test/java/com/techleads/app/MavenJenkinsIntegrationApplicationTests.java
+++ maven-jenkins-integration/src/test/java/com/techleads/app/MavenJenkinsIntegrationApplicationTests.java
@@ -1,5 +1,7 @@
 package com.techleads.app;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,8 @@ class MavenJenkinsIntegrationApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		 assertTrue( true );
 	}
 
 }
