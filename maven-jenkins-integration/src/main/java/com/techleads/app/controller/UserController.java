e43ba07fed2c5bb4bfb1834afc945fc9be978546
TechLeads <anupoju.anand@gmail.com>
2020-09-01 12:05:51 +0530

    Added Rest Controller (without jenkins setup)

---
+++ maven-jenkins-integration/src/main/java/com/techleads/app/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.techleads.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	@GetMapping(value = {"/"})
+	public String homePage() {
+		return "Welcome to User portal";
+	}
+
+}
